414c5e1e40976fa33223881e364ba2484cad0567
bhaktibuana <bhaktibuana19@gmail.com>
2022-01-11 22:11:45 +0700

    create alert component

--- client/src/pages/Homepage/Homepage.js
+++ client/src/pages/Homepage/Homepage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import HomepageContent from "../../components/HomepageContent/HomepageContent";
 import NavbarBs from "../../components/NavbarBs/NavbarBs";
 import "./Homepage.css";
 
@@ -7,6 +8,7 @@ const Homepage = () => {
     <>
       <div className="homepage-container">
         <NavbarBs />
+        <HomepageContent />
       </div>
     </>
   );
