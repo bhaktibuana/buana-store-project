c88c8df73836a9d8ad5ba5d1e30ae7c26f89ae16
bhaktibuana <bhaktibuana19@gmail.com>
2022-01-11 23:24:20 +0700

    create carousel

--- client/src/components/HomepageContent/HomepageContent.js
+++ client/src/components/HomepageContent/HomepageContent.js
@@ -1,18 +1,23 @@
 import React from "react";
 import AlertSuccess from "../AlertSuccess/AlertSuccess";
+import StoreCarousel from "../StoreCarousel/StoreCarousel";
 import "./HomepageContent.css";
 
 const HomepageContent = () => {
   return (
     <>
-      <div className="homepage-content-container">
-        <div className="homepage-alert-container">
-          <AlertSuccess />
+      <div className="homepage-carousel-container">
+        <StoreCarousel />
+      </div>
 
-          <div className="homepage-top-content-container">
-            <nav aria-label="breadcrumb"></nav>
-          </div>
+      <div className="homepage-content-container">
+        <div className="homepage-top-content-container">
+          <nav aria-label="breadcrumb"></nav>
         </div>
+
+        <div className="homepage-alert-container">
+            <AlertSuccess />
+          </div>
       </div>
     </>
   );
